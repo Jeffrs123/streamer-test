b71d455f4b3c427ef96d4c783c0367dde2533817
Jeffrs123 <rs.mensur@gmail.com>
2020-11-09 19:13:56 -0300

    filtro cursos e projetos

--- Streamer-App/src/app/eventos/eventos.component.ts
+++ Streamer-App/src/app/eventos/eventos.component.ts
@@ -4,43 +4,62 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.scss']
+  styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-
   eventosold: any = [
     {
       EventoId: 1,
       Tema: 'Angular',
-      Local: 'Belo Horizonte'
+      Local: 'Belo Horizonte',
     },
     {
       EventoId: 2,
       Tema: '.NET Core',
-      Local: 'São Paulo'
+      Local: 'São Paulo',
     },
     {
       EventoId: 3,
       Tema: 'Angular e .NET Core',
-      Local: 'Rio de Janeiro'
+      Local: 'Rio de Janeiro',
     },
-  ]
+  ];
 
-  projetos: any = [];
-  cursos: any = [];
+  
   imagemLargura: number = 50;
   imagemAltura: number = 50;
   imagemMargem: number = 20;
   mostrarImagemProjeto: boolean = false;
   mostrarImagemCurso: boolean = false;
-  filterProjetos = '';
-  filterCursos = '';
+  //filterProjetos = '';
+  
+  //filterCursos = '';
+  projetos: any = [];
+  projetosFiltrados: any = [];
+  _filterProjetos: string = '';
+  get filterProjetos(): string {
+    return this._filterProjetos;
+  }
+  set filterProjetos(value: string) {
+    this._filterProjetos = value;
+    this.projetosFiltrados = this.filterProjetos
+      ? this.filtrarProjetos(this.filterProjetos)
+      : this.projetos;
+  }
 
-
-  constructor(
-    private http: HttpClient
-  ) { }
-
+  cursos: any = [];
+  cursosFiltrados: any = [];
+  _filterCursos: string = '';
+  get filterCursos(): string {
+    return this._filterCursos;
+  }
+  set filterCursos(value: string) {
+    this._filterCursos = value;
+    this.cursosFiltrados = this.filterCursos
+      ? this.filtrarCursos(this.filterCursos)
+      : this.cursos;
+  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getCursos();
@@ -56,33 +75,40 @@ export class EventosComponent implements OnInit {
   }
 
   getProjetos() {
-    this.projetos = this.http
-      .get('http://localhost:5000/projetos')
-      .subscribe(
-        response => {
-          console.log('response', response);
-          this.projetos = response;
-        },
-        error => {
-          console.log('error', error)
-        }
-      )
-    ;
+    this.projetos = this.http.get('http://localhost:5000/projetos').subscribe(
+      (response) => {
+        console.log('response', response);
+        this.projetos = response;
+      },
+      (error) => {
+        console.log('error', error);
+      }
+    );
+  }
+  
+  filtrarProjetos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.projetos.filter(
+      projeto => projeto.whatWillWeDo.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  filtrarCursos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.cursos.filter(
+      curso => curso.name.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
   }
 
   getCursos() {
-    this.cursos = this.http
-      .get('http://localhost:5000/cursos')
-      .subscribe(
-        response => {
-          console.log('response', response);
-          this.cursos = response;
-        },
-        error => {
-          console.log('error', error)
-        }
-      )
-    ;
+    this.cursos = this.http.get('http://localhost:5000/cursos').subscribe(
+      (response) => {
+        console.log('response', response);
+        this.cursos = response;
+      },
+      (error) => {
+        console.log('error', error);
+      }
+    );
   }
-
 }
