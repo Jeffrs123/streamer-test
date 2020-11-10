97536076e73e997e33000884d0b6f9c428aee4df
Jeffrs123 <rs.mensur@gmail.com>
2020-11-09 23:37:06 -0300

    Domain - Classes e Relações para Udemy Course

---
+++ Streamer.Domain/Evento.cs
@@ -0,0 +1,28 @@
+//using System.ComponentModel.DataAnnotations;
+
+using System;
+using System.Collections.Generic;
+
+namespace Streamer.Domain
+{
+    public class Evento
+    {        //[Key]
+        public int Id { get; set; }
+        public string Local { get; set; }
+        public DateTime DateEvento { get; set; }
+        public string Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string ImageUrl { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public List<Lote> Lotes { get; set; }
+        public List<RedeSocial> RedesSociais { get; set; }
+        public List<PalestranteEvento> PalestranteEventos { get; set; }
+    }
+}
+
+
+//public enum ProjectStatus { get; set; }
+//public string Course { get; set; }
+//public Course CursO { get; set; }
+//public int CourseId { get; set; }
